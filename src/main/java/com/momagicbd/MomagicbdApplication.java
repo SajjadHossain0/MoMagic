f6449db62e51a7c08c5d9a39696441f6d857b9a7
mdsaj <mdsajjadhossain614@gmail.com>
2024-11-21 12:07:54 +0600

    new commit

---
+++ src/main/java/com/momagicbd/MomagicbdApplication.java
@@ -0,0 +1,22 @@
+package com.momagicbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomagicbdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MomagicbdApplication.class, args);
+    }
+
+}
+
+/*
+http://demo.webmanza.com/a55dbz923ace647v/api/v1.0/ping
+
+Content Retrieval
+Request Method: GET
+Request URL: http://demo.webmanza.com/a55dbz923ace647v/api/v1.0/services/content
+
+*/
---
+++ src/test/java/com/momagicbd/MomagicbdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.momagicbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomagicbdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
